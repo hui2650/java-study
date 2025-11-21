fd019e67974aab1508fc41bc21e071ad5f6ed6a8
taehui <hth4236727@gmail.com>
2025-11-21 18:14:45 +0900

    Update For.java

--- work/ex_1121/src/ch05/For.java
+++ work/ex_1121/src/ch05/For.java
@@ -5,32 +5,32 @@ import java.util.Scanner;
 public class For {
 	public static void main(String[] args) {
 
-//		// 1부터 10까지 출력하는 for문 작성하기
-//		// 가로로 출력 1 2 3 4 5 6 7 8 9 10
-//
+		// 1부터 10까지 출력하는 for문 작성하기
+		// 가로로 출력 1 2 3 4 5 6 7 8 9 10
+
 		for (int i = 1; i <= 10; i++) {
 			System.out.print(i + " ");
 		}
-//
-//		// 10부터 1까지 출력하기
+
+		// 10부터 1까지 출력하기
 		for (int i = 10; i >= 1; i--) {
 			System.out.print(i + " ");
 		}
-//
-//		// 3의 배수만 출력하기
+
+		// 3의 배수만 출력하기
 		for (int i = 10; i >= 1; i--) {
 			if (i % 3 == 0)
 				System.out.print(i + " ");
 		}
-//
-//		// 1부터 10까지의 총합 구하기
+
+		// 1부터 10까지의 총합 구하기
 		int sum = 0;
 		for (int i = 1; i <= 10; i++) {
 			sum += i;
 		}
 		System.out.println(sum);
-//
-//		// 키보드에서 숫자하나를 입력받고 구구단 출력하기
+
+		// 키보드에서 숫자하나를 입력받고 구구단 출력하기
 		Scanner sc = new Scanner(System.in);
 
 		int a;
@@ -40,11 +40,11 @@ public class For {
 			int result = a * i;
 			System.out.print(a + "x" + i + "=" + result + " ");
 		}
-//
-//		// 키보드에서 숫자 두개를 입력받아서
-//		// x부터 y까지의 총 합 구하기
-//		// x가 y보다 크면 값을 서로 교환해서 총합 구하기
-//
+
+		// 키보드에서 숫자 두개를 입력받아서
+		// x부터 y까지의 총 합 구하기
+		// x가 y보다 크면 값을 서로 교환해서 총합 구하기
+
 		int x, y;
 		x = sc.nextInt();
 		y = sc.nextInt();
@@ -62,10 +62,10 @@ public class For {
 		}
 
 		System.out.println(sum2);
-//
-//		// 1~20. 까지의 정수 중 2의 배수가 아니면서
-//		// 3의 배수가 아닌 수의 총합을 구하기
-//
+
+		// 1~20. 까지의 정수 중 2의 배수가 아니면서
+		// 3의 배수가 아닌 수의 총합을 구하기
+
 		int sum3 = 0;
 
 		for (int i = 1; i <= 20; i++) {
