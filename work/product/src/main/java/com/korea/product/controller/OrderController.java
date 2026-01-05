21815f52252ceb24f8d8024fd3f433968c9f32e4
taehui <hth4236727@gmail.com>
2026-01-05 18:11:21 +0900

    order 수정

--- work/product/src/main/java/com/korea/product/controller/OrderController.java
+++ work/product/src/main/java/com/korea/product/controller/OrderController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +36,21 @@ public class OrderController {
 							.build();
 		// http 200코드와 함께 body에 실린 응답(response)를 실어서 보낸다
 		return ResponseEntity.ok(response);
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> saveOrder(@RequestBody OrderDTO dto){
+		
+		
+		
+		List<OrderDTO> list = orderService.save(dto);
+		
+		ResponseDTO<OrderDTO> response = ResponseDTO.<OrderDTO>builder()
+							.data(list)
+							.build();
+		// http 200코드와 함께 body에 실린 응답(response)를 실어서 보낸다
+		return ResponseEntity.ok(response);
+		
 	}
 	
 }
--- work/product/src/main/java/com/korea/product/service/OrderService.java
+++ work/product/src/main/java/com/korea/product/service/OrderService.java
@@ -1,11 +1,15 @@
 package com.korea.product.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.korea.product.dto.OrderDTO;
+import com.korea.product.entity.ProductEntity;
+import com.korea.product.model.OrderEntity;
 import com.korea.product.persistence.OrderRepository;
+import com.korea.product.persistence.ProductRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,11 +19,37 @@ import lombok.RequiredArgsConstructor;
 public class OrderService {
 	
 	private final OrderRepository orderRepository;
+	private final ProductRepository productRepository;
 	
 	//주문내역 조회하기
 	public List<OrderDTO> getAllOrderTotalPrices(){
 		List<Object[]> results = orderRepository.findAllOrderTotalPrices();
 		//To
 		return OrderDTO.toListOrderDTO(results);
+	}
+	
+	// 주문하기
+	public List<OrderDTO> save(OrderDTO dto){
+		//productId, productCount
+		
+		// 1. 상품의 존재 여부 먼저 확인
+		ProductEntity option = productRepository.findById(dto.getProductId())
+				.orElseThrow(()-> new RuntimeException("상품이 존재하지 않습니다"));
+		
+		// 2. 재고가 있는지 확인
+		if(productEntity.getStock() < dto.getProductCount()) {
+			new RuntimeException("재고가 부족합니다, 현재 재고 : " + ProductEntity.getStock());
+		}
+		
+		// 주문하기
+		OrderEntity order = OrderEntity.builder()
+							.product(productEntity)
+							.productCount(dto.getProductCount())
+							.build();
+		
+		orderRepository.save(order);
+		
+		
+		
 	}
 }
